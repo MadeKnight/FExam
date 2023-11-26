a92790e7c705c0f658abe85553fee1d209e48f86
MadeKnight <142689532+MadeKnight@users.noreply.github.com>
2023-11-26 23:23:15 +0300

    задача

---
+++ Controller.cs
@@ -0,0 +1,42 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк 
+//формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+//Примеры:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+namespace ControlHomework
+{
+    static class Controller
+    {
+        private static IObjective _objective;
+        private static string _error = "Нет такой домашки";
+        private static int _choose_homework = 1;
+
+        private static void Main()
+        {
+            _objective = ObjSelector(_choose_homework);
+            _objective.Start();
+        }
+
+        private static IObjective ObjSelector(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return new ObjectiveOne();
+                default:
+                    Console.WriteLine(_error);
+                    return null;
+            }
+        }
+    }
+
+    public interface IObjective
+    {
+        void Start();
+    }
+}
---
+++ ObjectiveOne.cs
@@ -0,0 +1,87 @@
+﻿namespace ControlHomework
+{
+    public class ObjectiveOne : IObjective
+    {
+        private static readonly int _charLimit = 3;
+
+        private string _msgAnswerToController = "Запуск первого задания";
+        private string _msgWelcome = "Пишите любые слова";
+        private string _msgMassiveLength = "Сколько слов Вы хотите ввести?";
+        private string _msgNotANumber = "Введите число больше 0!";
+        private string _msgViewChosenWords = $"Выведем все слова, где количество символов не превышает {_charLimit}";
+        private string _msgNoResult = "Нечего выводить";
+
+        private string[] _words;
+
+        public void Start()
+        {
+            Console.WriteLine(_msgAnswerToController);
+            Calculation();
+        }
+
+        private void Calculation()
+        {
+            Console.WriteLine(_msgMassiveLength);
+            CheckOnNumber();
+            Console.WriteLine(_msgWelcome);
+            CreateArray();
+            Console.WriteLine(_msgViewChosenWords);
+            PrintResult(ChoosingWords());
+        }
+
+        private void CheckOnNumber()
+        {
+            if (int.TryParse(Console.ReadLine(), out int word) && word != 0)
+            {
+                _words = new string[word];
+            }                    
+            else
+            {
+                Console.WriteLine(_msgNotANumber);
+                CheckOnNumber();
+            }
+        }
+
+        private void CreateArray()
+        {
+            for (int i = 0; i < _words.Length; i++)
+            {
+                _words[i] = Console.ReadLine();
+            }
+        }                
+
+        private string[] ChoosingWords()
+        {
+            string[] tempArray = new string[_words.Length];
+            int i = 0;
+
+            foreach (string word in _words)
+            {
+                if (word.Length <= _charLimit)
+                {
+                    tempArray[i++] = word;
+                }
+            }
+
+            return tempArray;
+        }
+
+        private void PrintResult(string[] array)
+        {
+            string tempWord = _msgNoResult;
+            bool tempWordExist = false;
+
+            foreach (string word in array)
+            {
+                if (word != null)
+                {
+                    tempWord = word;
+                    Console.WriteLine(tempWord);
+                    tempWordExist = true;
+                }
+            }
+
+            if (!tempWordExist) Console.WriteLine(tempWord);
+        }
+    }
+}
